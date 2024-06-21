20447994a890bf52fd1f271551e31e54760811f7
Patrik Nordwall <patrik.nordwall@gmail.com>
2024-06-21 07:50:48 +0200

    feat: Projection with Flow, #24 (#43)

--- projection/src/main/scala/akka/projection/dynamodb/javadsl/DynamoDBProjection.scala
+++ projection/src/main/scala/akka/projection/dynamodb/javadsl/DynamoDBProjection.scala
@@ -9,8 +9,10 @@ import java.util.function.Supplier
 
 import scala.jdk.OptionConverters._
 
+import akka.Done
 import akka.actor.typed.ActorSystem
 import akka.annotation.ApiMayChange
+import akka.projection.ProjectionContext
 import akka.projection.ProjectionId
 import akka.projection.dynamodb.DynamoDBProjectionSettings
 import akka.projection.dynamodb.internal.DynamoDBTransactGroupedHandlerAdapter
@@ -19,11 +21,13 @@ import akka.projection.dynamodb.scaladsl
 import akka.projection.internal.GroupedHandlerAdapter
 import akka.projection.internal.HandlerAdapter
 import akka.projection.internal.JavaToScalaBySliceSourceProviderAdapter
+import akka.projection.javadsl.AtLeastOnceFlowProjection
 import akka.projection.javadsl.AtLeastOnceProjection
 import akka.projection.javadsl.ExactlyOnceProjection
 import akka.projection.javadsl.GroupedProjection
 import akka.projection.javadsl.Handler
 import akka.projection.javadsl.SourceProvider
+import akka.stream.javadsl.FlowWithContext
 
 @ApiMayChange
 object DynamoDBProjection {
@@ -92,7 +96,7 @@ object DynamoDBProjection {
     scaladsl.DynamoDBProjection
       .exactlyOnceGroupedWithin[Offset, Envelope](
         projectionId,
-        settings.asScala,
+        settings.toScala,
         JavaToScalaBySliceSourceProviderAdapter(sourceProvider),
         () => new DynamoDBTransactGroupedHandlerAdapter(handler.get()))(system)
       .asInstanceOf[GroupedProjection[Offset, Envelope]]
@@ -117,10 +121,45 @@ object DynamoDBProjection {
     scaladsl.DynamoDBProjection
       .atLeastOnceGroupedWithin[Offset, Envelope](
         projectionId,
-        settings.asScala,
+        settings.toScala,
         JavaToScalaBySliceSourceProviderAdapter(sourceProvider),
         () => new GroupedHandlerAdapter(handler.get()))(system)
       .asInstanceOf[GroupedProjection[Offset, Envelope]]
+  }
+
+  /**
+   * Create a [[akka.projection.Projection]] with a [[FlowWithContext]] as the envelope handler. It has at-least-once
+   * processing semantics.
+   *
+   * The flow should emit a `Done` element for each completed envelope. The offset of the envelope is carried in the
+   * context of the `FlowWithContext` and is stored in the database when corresponding `Done` is emitted. Since the
+   * offset is stored after processing the envelope it means that if the projection is restarted from previously stored
+   * offset then some envelopes may be processed more than once.
+   *
+   * If the flow filters out envelopes the corresponding offset will not be stored, and such envelope will be processed
+   * again if the projection is restarted and no later offset was stored.
+   *
+   * The flow should not duplicate emitted envelopes (`mapConcat`) with same offset, because then it can result in that
+   * the first offset is stored and when the projection is restarted that offset is considered completed even though
+   * more of the duplicated envelopes were never processed.
+   *
+   * The flow must not reorder elements, because the offsets may be stored in the wrong order and when the projection is
+   * restarted all envelopes up to the latest stored offset are considered completed even though some of them may not
+   * have been processed. This is the reason the flow is restricted to `FlowWithContext` rather than ordinary `Flow`.
+   */
+  def atLeastOnceFlow[Offset, Envelope](
+      projectionId: ProjectionId,
+      settings: Optional[DynamoDBProjectionSettings],
+      sourceProvider: SourceProvider[Offset, Envelope],
+      handler: FlowWithContext[Envelope, ProjectionContext, Done, ProjectionContext, _],
+      system: ActorSystem[_]): AtLeastOnceFlowProjection[Offset, Envelope] = {
+    scaladsl.DynamoDBProjection
+      .atLeastOnceFlow[Offset, Envelope](
+        projectionId,
+        settings.toScala,
+        JavaToScalaBySliceSourceProviderAdapter[Offset, Envelope](sourceProvider),
+        handler.asScala)(system)
+      .asInstanceOf[AtLeastOnceFlowProjection[Offset, Envelope]]
   }
 
 }
