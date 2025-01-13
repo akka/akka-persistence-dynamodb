066a060f0542dd7a5639d101531a054c56f2de8d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-01-13 01:55:24 +0100

    Update scalatest to 3.2.19 (#129)
    Co-authored-by: Peter Vlugter <59895+pvlugter@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -42,7 +42,7 @@ object Dependencies {
     val akkaJackson = "com.typesafe.akka" %% "akka-serialization-jackson" % AkkaVersion % Test
 
     val logback = "ch.qos.logback" % "logback-classic" % "1.5.16" % Test // EPL 1.0 / LGPL 2.1
-    val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12" % Test // ApacheV2
+    val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19" % Test // ApacheV2
     val junit = "junit" % "junit" % "4.12" % Test // Eclipse Public License 1.0
     val junitInterface = "com.novocode" % "junit-interface" % "0.11" % Test // "BSD 2-Clause"
 
