6afefba5f0651b80e70586bd255d18c66eaad7a5
Scala Steward <scala_steward@virtuslab.com>
2025-04-14 14:31:30 +0000

    bump: cloudwatch-metric-publisher, ... 2.31.21 (was 2.30.38)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   val AkkaProjectionVersionInDocs = VersionNumber(AkkaProjectionVersion).numbers match {
     case Seq(major, minor, _*) => s"$major.$minor"
   }
-  val AwsSdkVersion = "2.30.38"
+  val AwsSdkVersion = "2.31.21"
   // Java Platform version for JavaDoc creation
   // sync with Java version in .github/workflows/publish.yml#documentation
   val JavaDocLinkVersion = 17
